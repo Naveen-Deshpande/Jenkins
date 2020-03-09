25d4d9df941c15cfc1a79d95297cb4a9135349c7
Gururaj <gurumadhav08@gmail.com>
2020-03-09 12:01:11 +0000

    fourth commit by adding num4

--- Sample_Program.py
+++ Sample_Program.py
@@ -9,12 +9,15 @@ def myfunc():
 	num1 = 10
 	num2 = num1**2
 	num3 = num1 * num2
+	num4 = num3/num1
 
 	print(num1)
 	print('\n')
 	print(num2)
 	print('\n')
 	print(num3)
+	print('\n')
+	print(num4)
 	print('\n')
 
 

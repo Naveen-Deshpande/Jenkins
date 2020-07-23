4da783657b4312bac87c1c699bff0a9c952c72c4
Gururaj <gurumadhav08@gmail.com>
2020-07-23 14:32:32 +0100

    Print statement changes in num_op

--- num_op.py
+++ num_op.py
@@ -4,17 +4,15 @@ def add_num(a,b):
 
 
 def multiply_num(a,b):
-<<<<<<< HEAD
 	return (a * b)
 
 
 def sub_num(a,b):
 	return (a - b)
-=======
 
 
 res1 = add_num(10,10)
 res2 = multiply_num(10,6)
-res3 = add_num(50,50)
-print("Addition - {}, Multiplication - {}".format(res1,res2))
->>>>>>> 986b7f6c32c24e531726a5a0a41a258aca17faa0
+res3 = sub_num(50,20
+
+print("Addition - {}, Multiplication - {}, Substraction - {}".format(res1,res2,res3))

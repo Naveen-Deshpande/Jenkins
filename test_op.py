98e0dfcc234f9bb2ed08e0fd36421fd004ee7951
Gururaj <gurumadhav08@gmail.com>
2020-07-23 14:18:49 +0100

    Added sub function to num_op and sub test case to test_op

--- test_op.py
+++ test_op.py
@@ -19,6 +19,12 @@ class TestOperation(unittest.TestCase):
 		result = num_op.multiply_num(num1,num2)
 		self.assertEqual(result, 20)
 
+	def test_sub(self):
+		num1 = 50
+		num2 = 20
+		result = num_op.sub_num(num1,num2)
+		self.assertEqual(result,30)
+
 
 if __name__ == '__main__':
 	unittest.main()
